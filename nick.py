18206664c2b79e3dddef2824df6193202ad9e9ee
NicolasCosta <nicolas.costa41@hotmail.com>
2024-09-09 09:18:23 -0300

    Add files via upload

---
+++ nick.py
@@ -0,0 +1,15 @@
+# Estrutura de Descisão 
+
+i = int(input("Digite sua idade:"))
+
+if i>10:
+    print("Você é uma criança")
+
+elif i>=10 and i<20:
+    print("Você é um adolecente")
+
+elif i>=20 and i<30:
+    print("Você é um Jovem")
+
+elif i>=30 and i<=100:
+    print("Erro, tente novamente!")
